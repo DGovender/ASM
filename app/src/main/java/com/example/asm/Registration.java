1f1287e0df83c95bdd823316e4d49be3511b005b
Darien Govender <darien.govender@live.co.za>
2018-05-21 01:06:42 +0200

    Registration Design Completed && Intent Connected

--- app/src/main/java/com/example/asm/Registration.java
+++ app/src/main/java/com/example/asm/Registration.java
@@ -1,12 +1,12 @@
- package com.example.asm;
+package com.example.asm;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.TextView;
 
- public class Login extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +15,15 @@ import android.widget.TextView;
         //Change App Theme to Remove Title Bar
         setTheme(R.style.AppThemeForCredentials);
 
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
 
-        //Move Intent from Login to Registration
-        TextView register = findViewById(R.id.txtViewRegister);
+        //Move Intent from Registration Back to Login
+        CardView register = findViewById(R.id.cardViewRegister);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Registration.class);
+                Intent intent = new Intent(Registration.this, Login.class);
                 startActivity(intent);
             }
         });
