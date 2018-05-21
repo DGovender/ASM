f1a30fb11d7bbc45178e4fd3b2702343b4723500
Darien Govender <darien.govender@live.co.za>
2018-05-21 05:00:47 +0200

    Navigation Bar Functionality Implemented

--- app/src/main/java/com/example/asm/MainActivity.java
+++ app/src/main/java/com/example/asm/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.asm;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,8 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +23,43 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationview = findViewById(R.id.nav_view);
+        navigationview.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Match_Centre()).commit();
+            navigationview.setCheckedItem(R.id.nav_match_centre);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_match_centre:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Match_Centre()).commit();
+                break;
+            case R.id.nav_players:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Players()).commit();
+                break;
+            case R.id.nav_teams:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Teams()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Settings()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/example/asm/Teams.java
@@ -0,0 +1,22 @@
+package com.example.asm;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Darien Govender on 5/21/2018.
+ */
+
+public class Teams extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_teams, container, false);
+    }
+}
